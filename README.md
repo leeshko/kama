ce7c7f7f995c021d5b50132dd7de895745ab504e
Dzmitry Lishko <d.lishko@godeltech.com>
2022-07-20 16:13:32 +0200

    simple express 2 les

--- README.md
+++
@@ -1,2 +0,0 @@
-# kama
-backEnd
---
+++ dist/index.js
@@ -0,0 +1,48 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3000;
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'devops' },
+        { id: 3, title: 'back-end' },
+        { id: 4, title: 'react' },
+    ],
+};
+app.get('/', (req, res) => res.send({ message: 'Hello World!!!' }));
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses;
+    if (req.query.title) {
+        foundCourses = foundCourses.filter((course) => course.title.includes(req.query.title));
+    }
+    res.json(foundCourses);
+});
+app.post('/courses', (req, res) => {
+    const newCourse = {
+        id: db.courses.length + 1,
+        title: req.body.title,
+    };
+    db.courses.push(newCourse);
+    res.status(201).json(newCourse);
+});
+app.get('/courses/:id', (req, res) => {
+    const findCourse = db.courses.find((course) => course.id === +req.params.id);
+    if (!findCourse) {
+        res.sendStatus(404).json({ message: 'Course not found' });
+        return;
+    }
+    res.json(findCourse);
+});
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter((course) => course.id !== +req.params.id);
+    res.sendStatus(204);
+});
+// app.get('/sa', (req: any, res: any) => {
+//   console.log(req);
+//   res.send('Hello SA');
+// });
+// app.post('/sa', (req: any, res: any) => res.send('Create SA'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
